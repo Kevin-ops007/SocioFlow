87f38dcd1301bd73f5ce6cc92693979cbcb90d57
kevin <kmathew@peloton-technologies.com>
2021-09-23 21:12:55 -0700

    final

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,7 @@ import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
 
 const main = async ()=>{
-    const orm = await MikroORM.init({ 
-        entities: [Post],
-        dbName: 'socioflow_db',
-        type: 'postgresql',
-        debug: !__prod__
-    })
+    const orm = await MikroORM.init(mikroConfig)
 
     const post = orm.em.create(Post, {title: 'my first post'})
     await orm.em.persistAndFlush(post)
