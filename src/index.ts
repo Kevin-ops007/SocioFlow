d6c58550de6f039dec3aff058c0449f5ad726265
kevin <kmathew@peloton-technologies.com>
2021-10-04 22:08:38 -0700

    more fixing

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,11 @@ import { __prod__ } from "./constants"
 import { Post } from "./entities/Post";
 //import mikroConfig from "./mikro-orm.config";
 import { Comment } from "./entities/Comment";
+import path from "path";
+import mikroOrmConfig from "./mikro-orm.config";
 
 const main = async ()=>{
-    const orm = await MikroORM.init( {
-        entities: [Post],
-        dbName: 'socioflow_db',
-        type: 'postgresql',
-        debug: !__prod__
-    }
-    )
+    const orm = await MikroORM.init( mikroOrmConfig)
 
     const post = orm.em.create(Post, {title: 'my first post'})
     await orm.em.persistAndFlush(post)
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,9 +1,15 @@
-import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+import { __prod__ } from "./constants"
+import {MikroORM } from "@mikro-orm/core"
+import { Post } from "./entities/Post"
+import path from "path"
 
-export default {
+export default{
+    migrations:{
+        path: path.join(__dirname, "./migration"),
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
     entities: [Post],
     dbName: 'socioflow_db',
     type: 'postgresql',
     debug: !__prod__
-} as const
+}as Parameters<typeof MikroORM.init>[0]
