b96417207a54fce3a7d9092dcffccb73f6808e7c
kevin <kmathew@peloton-technologies.com>
2021-10-04 21:41:17 -0700

    fixing some small problems

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,20 @@
-console.log("Hello World");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const constants_1 = require("./constants");
+const Post_1 = require("./entities/Post");
+const Comment_1 = require("./entities/Comment");
+const main = async () => {
+    const orm = await core_1.MikroORM.init({
+        entities: [Post_1.Post],
+        dbName: 'socioflow_db',
+        type: 'postgresql',
+        debug: !constants_1.__prod__
+    });
+    const post = orm.em.create(Post_1.Post, { title: 'my first post' });
+    await orm.em.persistAndFlush(post);
+    const comment = orm.em.create(Comment_1.Comment, { title: 'my first commentt' });
+    await orm.em.persistAndFlush(comment);
+};
+main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants"
 import { Post } from "./entities/Post";
-import mikroConfig from "./mikro-orm.config";
-import { Comment } from "./entities/Post";
+//import mikroConfig from "./mikro-orm.config";
+import { Comment } from "./entities/Comment";
 
 const main = async ()=>{
-    const orm = await MikroORM.init(mikroConfig)
+    const orm = await MikroORM.init( {
+        entities: [Post],
+        dbName: 'socioflow_db',
+        type: 'postgresql',
+        debug: !__prod__
+    }
+    )
 
     const post = orm.em.create(Post, {title: 'my first post'})
     await orm.em.persistAndFlush(post)
